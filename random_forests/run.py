333ec76dea46adc303a78f822d22620a2f45e72f
Matheus Westhelle <matheus.westhelle@gmail.com>
2020-10-12 17:08:26 -0300

    Make test script use the Random Forest model

--- random_forests/run.py
+++ random_forests/run.py
@@ -6,5 +6,6 @@ from kfold_crossvalidation import KFoldCrossValidation
 
 if __name__ == "__main__":
     model = RandomForest(10, DecisionTree())
+    # model = DecisionTree()
     kfold = KFoldCrossValidation(model, delimiter=";")
     kfold.kfold_cross_validation(os.path.join(os.getcwd(), "../data", "benchmark.csv"))
